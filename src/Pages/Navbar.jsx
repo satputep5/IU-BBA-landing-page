875e92ec78c7c931f8a732e49b4d9f18a88de479
Techwolf78 <mrajaypawar207@gmail.com>
2025-04-05 16:20:18 +0530

    Your commit message describing the changes

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -73,7 +73,7 @@ function Navbar() {
               <img
                 src={logo}
                 alt="Logo"
-                className="h-auto max-w-20"
+                className="h-auto max-w-28"
               />
               <img
                 src={excellenceLogo}
